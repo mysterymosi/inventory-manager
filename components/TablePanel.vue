dd19e59c271e6f006069adb7211b0b4a00e1cad9
Odey Okimmosi <okimmosimiracle@gmail.com>
2020-02-21 14:38:55 +0100

    added spreadjs

---
+++ components/TablePanel.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="col-sm-12 tablePanel">
+    <div class="card dashboardRow">
+      <div class="card-body">
+        <h5 class="card-title">{{title}}</h5>
+        <slot></slot>
+        <AppButton type="button" @click="$emit('onEndTheDay')" class="btn btn-success">END THE DAY</AppButton>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import AppButton from '~/components/UI/AppButton'
+export default {
+  props: ["title"]
+}
+</script>
